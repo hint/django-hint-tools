203892acc337ae808729fded54d6a03e987e5016
centralniak <p.kilczuk@neumea.pl>
2012-03-13 17:27:35 +0100

    Remove hardcoded data

--- djtools/data/skel/_djtools_common.py
+++ djtools/data/skel/_djtools_common.py
@@ -9,5 +9,5 @@ LOCAL_MEDIA_DIR = os.path.join(LOCAL_PROJECT_ROOT, 'media')
 # SSH production environment
 SSH_USER = 'user'
 SSH_HOST = 'ssh-host'
-SSH_PROJECT_DIR = '/data/stor/www/olimpiadapyth/olimpiada.pyth.hint.pl/www/'
+SSH_PROJECT_DIR = '/home/ssh-user/project/dir/'
 SSH_MEDIA_DIR = SSH_PROJECT_DIR + 'media'
