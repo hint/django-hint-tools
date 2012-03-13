551a0e207fa880bf1c6962a85a4b0d6ac9a62224
centralniak <p.kilczuk@neumea.pl>
2012-03-13 16:25:13 +0100

    Fix getting skeleton directory

--- scripts/djbuildhere.py
+++ scripts/djbuildhere.py
@@ -7,8 +7,8 @@ Build
 import os
 import shutil
 
-PACKAGE_ROOT = os.path.abspath(os.path.dirname(__file__))
+import djtools
 
 # would be good not to overwrite old _djtools_common.py
 
-shutil.copy(os.path.join(PACKAGE_ROOT, 'data', 'skel', '*'), os.getcwd())
+shutil.copy(djtools.get_skel_dir(), os.getcwd())
--- src/djtools/__init__.py
+++ src/djtools/__init__.py
@@ -1,5 +1,8 @@
 import os
 
+def get_skel_dir():
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'skel'))
+
 def rsync(basedir, src, dest):
     #rsync --checksum --compress --exclude '.svn' --links --perms --progress --recursive --update --verbose $BASEDIR$1 $2
     print basedir, src, dest
