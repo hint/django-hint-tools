55b1e948dd276cb5abef66b23d3f0127e6fd4c3a
centralniak <p.kilczuk@stylesheet.pl>
2012-03-13 15:32:15 +0100

    Initial script commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-centralniak-tools',
+      version='0.1',
+      description='Package of handy django console tools',
+      author='Piotr Kilczuk',
+      author_email='p.kilczuk@neumea.pl',
+      url='https://github.com/centralniak/django-centralniak-tools',
+
+      # unsure...
+      packages=['djtools'],
+
+      
+     )
