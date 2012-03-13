0f729d07c6501fe5cb1904ec72264694a5e9023c
centralniak <p.kilczuk@neumea.pl>
2012-03-13 17:27:50 +0100

    Fix some bugs in djbuildhere

--- scripts/djbuildhere.py
+++ scripts/djbuildhere.py
@@ -9,14 +9,19 @@ import os
 import shutil
 import sys
 
-import djtools
+try:
+    import djtools
+# this is for testing purposes
+except ImportError:
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+    import djtools
 
 # would be good not to overwrite old _djtools_common.py
 
-if len(sys.argv > 1):
+if len(sys.argv) > 1:
     destdir = sys.argv[1]
 else:
     destdir = os.getcwd()
 
-for file in glob(djtools.get_skel_dir() + '/*'):
+for file in glob.glob(djtools.get_skel_dir() + '/*'):
     shutil.copy(file, destdir)
