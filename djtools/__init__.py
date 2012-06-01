17d795618e91dce347a6da3a6e47596b3902077f
centralniak <p.kilczuk@neumea.pl>
2012-06-01 17:20:36 +0200

    Remove old approach files

--- djtools/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def get_skel_dir():
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), 'data', 'skel'))
-
-def rsync(basedir, src, dest):
-    #rsync --checksum --compress --exclude '.svn' --links --perms --progress --recursive --update --verbose $BASEDIR$1 $2
-    print basedir, src, dest
-    return 
